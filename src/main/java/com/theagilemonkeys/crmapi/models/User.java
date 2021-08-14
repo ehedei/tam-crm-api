eeeef57fd17dcc1c1570ed7d787d689fcd25be11
ehedei <ehedeidev@gmail.com>
2021-08-14 20:50:21 +0100

    User Model: Added Unique Indexes to Email and Username

--- src/main/java/com/theagilemonkeys/crmapi/models/User.java
+++ src/main/java/com/theagilemonkeys/crmapi/models/User.java
@@ -4,18 +4,20 @@ import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 @Data
 public class User {
-    @Id
+  @Id
   private String id;
 
+  @Indexed(unique = true)
   private String username;
 
-
+  @Indexed(unique = true)
   private String email;
 
   private String password;
