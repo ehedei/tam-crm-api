b9fef8363aaf5f7e3c878842b822b498ff746a38
ehedei <ehedeidev@gmail.com>
2021-08-15 20:48:28 +0100

    Password Encoder: Added

--- src/main/java/com/theagilemonkeys/crmapi/config/security/WebSecurityConfig.java
+++ src/main/java/com/theagilemonkeys/crmapi/config/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.theagilemonkeys.crmapi.config;
+package com.theagilemonkeys.crmapi.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
